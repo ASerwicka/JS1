834c4f37ed8998224e0fddffeac249cdce16782c
ASerwicka <aleserwicka@gmail.com>
2022-06-03 11:11:12 +0200

    add TODO section

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import data
 
 EDUCATION = 'Wszystkie'
-DEPENDENCY = 'Ogólnie'
+DEPENDENCY = 'Ogółem'
 
 
 class Gui(Tk):
@@ -17,8 +17,8 @@ class Gui(Tk):
         # labels from csv file
         self.labels = self.Data.get_labels()
         self.local_labels = self.labels
-        self.chosen_dependency = DEPENDENCY
-        self.chosen_education = EDUCATION
+        self.chosen_dependency = None
+        self.chosen_education = None
         self.combo_dep = None
         self.combo_edu = None
         self.geometry("900x500")
@@ -26,11 +26,11 @@ class Gui(Tk):
         self.upper_frame.pack(fill=BOTH, expand=YES, side=TOP)
         self.upper_frame.pack_propagate(False)
 
-        self.up_left_frame = LabelFrame(self.upper_frame, bg="green", height=400, width=250)
+        self.up_left_frame = LabelFrame(self.upper_frame, height=400, width=250)
         self.up_left_frame.pack(fill=BOTH, side=LEFT)
         self.up_left_frame.pack_propagate(False)
 
-        self.up_right_frame = LabelFrame(self.upper_frame, bg="blue", height=400, width=650)
+        self.up_right_frame = LabelFrame(self.upper_frame, height=400, width=650)
         self.up_right_frame.pack(fill=BOTH, expand=YES, side=RIGHT)
         self.up_right_frame.pack_propagate(False)
 
@@ -43,11 +43,13 @@ class Gui(Tk):
         self.chosen_dependency = self.combo_dep.get()
 
     def create_combobox_education(self):
-        self.combo_edu = ttk.Combobox(self.up_left_frame, values=self.labels)
+        temp = self.labels[1:]
+        temp.append('Wszystkie',)
+        self.combo_edu = ttk.Combobox(self.up_left_frame, values=temp)
         self.combo_edu.set("Pick an education")
         self.combo_edu.pack(fill=X)
 
-        self.combo_dep = ttk.Combobox(self.up_left_frame, values=('Wiek', 'Płeć'))
+        self.combo_dep = ttk.Combobox(self.up_left_frame, values=('Wiek', 'Ogółem'))
         self.combo_dep.set("Pick a dependency")
         self.combo_dep.pack(fill=X)
 
@@ -55,14 +57,41 @@ class Gui(Tk):
         button.pack(fill=X)
 
     def get_data_to_display(self):
-        if self.chosen_dependency.__eq__('Ogólnie') and self.chosen_education.__eq__('Wszystkie'):
+        if self.chosen_dependency is None and self.chosen_education is None:
+            self.view_data(None)
+        elif self.chosen_dependency.__eq__('Ogółem') and self.chosen_education.__eq__('Wszystkie'):
             self.view_data(self.Data.get_general_data())
+        elif self.chosen_dependency.__eq__('Wiek'):
+            if self.chosen_education.__eq__('Ogółem'):
+                #TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Podstawowe'):
+                # TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Średnie'):
+                # TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Policealne'):
+                # TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Zasadnicze zawodowe'):
+                # TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Gimnazjalne'):
+                # TODO
+                self.view_data(None)
+            elif self.chosen_education.__eq__('Wyższe'):
+                # TODO
+                self.view_data(None)
 
     # require pandas dataframe
     def view_data(self, dataframe):
         if dataframe is not None:
             self.local_labels = dataframe.columns.tolist()
             self.local_data = dataframe.values.tolist()
+        else:
+            self.local_labels = self.labels
+            self.local_data = self.data
         tree = ttk.Treeview(self.bottom_frame, columns=self.local_labels, show='headings', selectmode='browse')
         scrollbar = ttk.Scrollbar(self.bottom_frame, orient="vertical", command=tree.yview)
         scrollbar.pack(side='right', fill='y')
